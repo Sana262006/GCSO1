6a561cf874fe4fd2ab79bea26220d8e55f5d1a1e
Saniya <74128070+Sana262006@users.noreply.github.com>
2021-05-13 17:14:06 +0530

    Add files via upload

---
+++ mylibrary.js
@@ -0,0 +1,37 @@
+function istouching(movingRect,fixedRect)
+{
+  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+    && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2
+    && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
+    && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2) 
+    {
+  //movingRect.shapeColor = "red";
+  //fixedRect.shapeColor = "red";
+
+  return true;
+}
+else {
+  //movingRect.shapeColor = "green";
+  //fixedRect.shapeColor = "green";
+
+  return false;
+}
+
+}
+
+
+
+function Bounceoff(movingRect,fixedRect)
+{
+  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+    && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2) 
+    {
+  movingRect.velocityX = movingRect.velocityX * (-1);
+  fixedRect.velocityX = fixedRect.velocityX * (-1);
+  }
+  if (movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
+  && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2)
+  {
+  movingRect.velocityY = movingRect.velocityY * (-1);
+  fixedRect.velocityY = fixedRect.velocityY * (-1);
+  }
